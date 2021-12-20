8fbe754e32992506cb931f484f7436370edcd8c9
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-20 13:32:19 +0100

    con estos parametros le doy color y cierro turtle

--- estrellaregular.py
+++ estrellaregular.py
@@ -1,3 +1,9 @@
-import turtle
+import turtle #es como un puntero que se va desplazando y dibujando a su vez
 
 def estrella():
+    turtle.fillcolor("yellow")
+    turtle.begin_fill()
+    
+    
+    turtle.end_fill()
+turtle.end_fill()
