ae52524da183c031a7054523f3fd602200870cb5
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-20 14:50:24 +0100

    consigo estrella pero tiene q elegir las puntas :/

--- estrellaregular.py
+++ estrellaregular.py
@@ -1,14 +1,17 @@
 import turtle #es como un puntero que se va desplazando y dibujando a su vez
+turtle.begin_fill()
 
 def estrella():
+    turtle.color('grey')
     turtle.fillcolor("yellow")
     turtle.begin_fill()
-    
+
     while True:
         turtle.forward(200)
         turtle.left(200)
-            
-    turtle.end_fill()
-turtle.end_fill()
+        if abs(turtle.pos()) < 1: #para que el puntero se pare una vez dibujado toda la estrella
+            break
+    turtle.end_fill()        
 
 estrella()
+turtle.done() #para que no se cierre la pantalla turtle
