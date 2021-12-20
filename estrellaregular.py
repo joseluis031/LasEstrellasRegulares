483e9bdc93a0382337da681fae255950ac7c9dba
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-20 14:30:08 +0100

    bucle q me da estrellaperosincolorypuntero no para

--- estrellaregular.py
+++ estrellaregular.py
@@ -4,6 +4,11 @@ def estrella():
     turtle.fillcolor("yellow")
     turtle.begin_fill()
     
-    
+    while True:
+        turtle.forward(200)
+        turtle.left(200)
+            
     turtle.end_fill()
 turtle.end_fill()
+
+estrella()
